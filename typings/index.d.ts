afeab1e8ddf4772cb3f7bbde3e7b5638dcddeb89
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-05-16 19:41:37 +0200

    :hammer: chore: remove typings because typescript does that

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "ctgs.js" {
-    export default class shortener {
-        constructor() {}
-        async new(slug: string, redirect_url: string)
-        async get(slug: string)
-    }
-}
